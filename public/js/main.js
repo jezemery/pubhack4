a192676970d1134f4810b0d36ec43b972dbee125
Jez Emery <jez.emery@gmail.com>
2015-06-25 12:23:56 +0100

    Added shit font kerning and bad stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,37 @@
 $(function () {
 
-	setTimeout(function(){
-		$(".bootscreen").css("display", "none");
-	},3000);
+    setTimeout(function () {
+        $(".bootscreen").css("display", "none");
+    }, 3000);
 
-	setTimeout(function(){
-		$(".bsod").css("display", "block");
-	},10000);
+    //setTimeout(function(){
+    //	$(".bsod").css("display", "block");
+    //},10000);
 
-	$(".icons li").click(function(e){
-		e.preventDefault();
-		$(".icons li").removeClass("active");
-		$(this).toggleClass("active");
-	});
+    $(".icons li").click(function (e) {
+        e.preventDefault();
+        $(".icons li").removeClass("active");
+        $(this).toggleClass("active");
+    });
 
-	$( ".icons li.game" ).dblclick(function() {
-	    $("body").css("cursor", "url(../img/cursor.png),wait");
+    $(".icons li.game").dblclick(function () {
+        $("body").css("cursor", "url(../img/cursor.png),wait");
 
-		setTimeout(function(){
-	     // toggle back after 1 second
-	     $("body").css("cursor", "url(../img/cursor-normal.png),wait");
-	     $(".window").addClass("active");
-	   },4000);
-	});
+        setTimeout(function () {
+            // toggle back after 1 second
+            $("body").css("cursor", "url(../img/cursor-normal.png),wait");
+            $(".window").addClass("active");
+        }, 4000);
+    });
+    var currentdate = new Date();
+    var hours = new Date().getHours() % 12;
+    if (hours == 0) {
+        hours += 12;
+    }
+    var time = 'am';
+    if (currentdate.getHours() >= 12) {
+        time = 'pm';
+    }
+    $('.time span').html(hours + ":" + currentdate.getMinutes() + ' ' + time.toUpperCase());
 
 });
