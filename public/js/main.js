7c9316e1fc6300dce85949ea8a4f1c3aff828db1
Harry Wiseman <harry@strawberrysoup.co.uk>
2015-06-25 11:01:24 +0100

    lag added

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,17 @@ $(function () {
 		$(".icons li").removeClass("active");
 		$(this).toggleClass("active");
 	});
+
+	$( ".icons li" ).dblclick(function() {
+		setTimeout(function(){
+	     // toggle back after 1 second
+	     $("body").css("cursor", "url(../img/cursor.png),wait");
+	   },2000);
+
+		setTimeout(function(){
+	     // toggle back after 1 second
+	     $("body").css("cursor", "url(../img/cursor-normal.png),wait");
+	   },4000);
+	});
+
 });
